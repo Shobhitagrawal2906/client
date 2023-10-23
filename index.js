0b462dde58eca3a70b487f82f5afb4805e9c9d80
Shobhit Gupta <shree7sargam@gmail.com>
2023-10-23 23:20:54 +0530

    testing

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const nodemailer = require('nodemailer');
+// const cors = require("cors");
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5500",
+//     "http://localhost:3000",
+//     "http://localhost:5000",
+//     "http://127.0.0.1:5500",
+//   ],
+//   credentials: true,
+// })
+// );
+
+app.use(express.json());
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/shopping.html'); // Replace 'index.html' with your HTML file's name and path
+});
+
+app.get("/test",(req,res)=>{
+    res.send("Testing")
+  })
+
+
+app.post('/mailsender',  (req, res) => {
+    // console.log("DATA :", req.body)
+    // res.send("Done")
+      try {
+          const {subject,message}=req.body;
+          const mail=process.env.MAIL_ID;
+          const sendmail=process.env.MAIL_TO;
+          const password= process.env.MAIL_PASSWORD;
+          
+          // console.log(subject,message,mail,sendmail,password)
+          var transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: mail,
+                pass: password
+              }
+            });
+            var mailOptions = {
+              from: mail,
+              to: sendmail,
+              subject: subject,
+              text: message
+            };
+            transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+                  res.status(500).send(error);
+              } else {
+                  res.status(200).send('Email sent: ' + info.response);
+              }
+            });
+      } catch (error) {
+          res.status(500).send({"error":error});
+        }
+  });
+  
+
+  app.use((req, res, next) => {
+      const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+      // console.log(req.headers['x-forwarded-for'], " : ",req.connection.remoteAddress," , ",`User IP: ${clientIp}`)
+      // const clientIp = req.ip; // Get the user's IP address from the request
+      console.log(`User IP: ${clientIp}`);
+      next(); // Call the next middleware in the chain
+    });
+    
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
+
+// const path=require("path");
+
+//   app.use(express.static('client/build'));
+//    app.get('*', (req, res) => {
+//           res.sendFile(path.resolve('client','build','index.html'));
+//   });
+  
